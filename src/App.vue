f3fa5cda625cfceead6a9073f0c26f1c0fb1d49e
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-01-31 13:21:08 +0100

    Added: HeaderBox. Installed: axios&Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
   <div id="app">
-    
+    <header-box />
+    <main-content />
+  
   </div>
 </template>
 
 <script>
+import HeaderBox from './components/HeaderBox.vue';
+import MainContent from './components/MainContent.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    MainContent,
+    HeaderBox,
+  },
+  data(){
+    return {
+      dischi: []
+    }
+  },
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
+      this.dischi = response.data
+    })
+  },
 }
 </script>
 
---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <div class="logo-img">
+            <img src="../assets/spotify-logo.png" alt="">
+        </div>
+        
+    </header>
+</template>
+
+<script>
+
+
+export default {
+ 
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+header{
+    height: 80px;
+    background-color: #282828;
+    
+    
+
+
+    .logo-img{
+    width: 50px;
+            
+        img{
+            width: 100%;
+            padding: 15px;
+        }
+    }
+}
+
+</style>
